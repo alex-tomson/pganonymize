35b5910b2cb0cd7a5f343a2a89dc096154c44faf
alex-tomson <alex@bioiq.io>
2022-09-21 15:14:49 +0300

    added tests; pre-commit config

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,93 +4,169 @@ from argparse import Namespace
 import pytest
 from mock import Mock, call, patch
 
-from tests.utils import quote_ident
-
 from pganonymize.cli import get_arg_parser, main
+from pganonymize.exceptions import BadSchemaFormat
+from tests.utils import quote_ident
 
 
 class TestCli:
-    @patch('psycopg2.extensions.quote_ident', side_effect=quote_ident)
-    @patch('pganonymize.utils.psycopg2.connect')
-    @patch('pganonymize.utils.subprocess')
-    @pytest.mark.parametrize('cli_args, expected, expected_executes, commit_calls, call_dump', [
+    @patch("psycopg2.extensions.quote_ident", side_effect=quote_ident)
+    @patch("pganonymize.utils.psycopg2.connect")
+    @patch("pganonymize.utils.subprocess")
+    @pytest.mark.parametrize(
+        "cli_args, expected, expected_executes, commit_calls, call_dump",
         [
-            '--host localhost --port 5432 --user root --password my-cool-password --dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql "set work_mem=\'1GB\'"',
-            # noqa
-            Namespace(verbose=1, list_providers=False, schema='./tests/schemes/valid_schema.yml', dbname='db',
-                      user='root',
-                      password='my-cool-password', host='localhost', port='5432', dry_run=False, dump_file=None,
-                      init_sql="set work_mem='1GB'"),  # noqa
-            [call('SET search_path TO db;'),
-             call("set work_mem='1GB'"),
-             call('TRUNCATE TABLE "django_session"'),
-             call('SELECT COUNT(*) FROM "auth_user"'),
-             call('SELECT "id", "first_name", "last_name", "email" FROM "auth_user"'),
-             call(
-                 'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n                    FROM "auth_user" WITH NO DATA'),
-             # noqa
-             call('CREATE INDEX ON "tmp_auth_user" ("id")'),
-             call(
-                 'UPDATE "auth_user" t SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" FROM "tmp_auth_user" s WHERE t."id" = s."id"')
-             # noqa
-             ],
-            1,
-            []
-        ],
-        [
-            '--dry-run --host localhost --port 5432 --user root --password my-cool-password --dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql "set work_mem=\'1GB\'"',
-            # noqa
-            Namespace(verbose=1, list_providers=False, schema='./tests/schemes/valid_schema.yml', dbname='db',
-                      user='root',
-                      password='my-cool-password', host='localhost', port='5432', dry_run=True, dump_file=None,
-                      init_sql="set work_mem='1GB'"),  # noqa
-            [call('SET search_path TO db;'),
-             call("set work_mem='1GB'"),
-             call('TRUNCATE TABLE "django_session"'),
-             call('SELECT "id", "first_name", "last_name", "email" FROM "auth_user" LIMIT 100'),
-             call(
-                 'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n                    FROM "auth_user" WITH NO DATA'),
-             # noqa
-             call('CREATE INDEX ON "tmp_auth_user" ("id")'),
-             call(
-                 'UPDATE "auth_user" t SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" FROM "tmp_auth_user" s WHERE t."id" = s."id"')
-             # noqa
-             ],
-            0, []
-        ],
-        [
-            '--dump-file ./dump.sql --host localhost --port 5432 --user root --password my-cool-password --dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql "set work_mem=\'1GB\'"',
-            # noqa
-            Namespace(verbose=1, list_providers=False, schema='./tests/schemes/valid_schema.yml', dbname='db',
-                      user='root',
-                      password='my-cool-password', host='localhost', port='5432', dry_run=False, dump_file='./dump.sql',
-                      init_sql="set work_mem='1GB'"),  # noqa
             [
-                call('SET search_path TO db;'),
-                call("set work_mem='1GB'"),
-                call('TRUNCATE TABLE "django_session"'),
-                call('SELECT COUNT(*) FROM "auth_user"'),
-                call('SELECT "id", "first_name", "last_name", "email" FROM "auth_user"'),
-                call(
-                    'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n                    FROM "auth_user" WITH NO DATA'),
-                # noqa
-                call('CREATE INDEX ON "tmp_auth_user" ("id")'),
-                call(
-                    'UPDATE "auth_user" t SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" FROM "tmp_auth_user" s WHERE t."id" = s."id"')
-                # noqa
+                "--host localhost --port 5432 --user root --password my-cool-password --dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql \"set work_mem='1GB'\"",  # noqa
+                Namespace(
+                    verbose=1,
+                    list_providers=False,
+                    schema="./tests/schemes/valid_schema.yml",
+                    dbname="db",
+                    user="root",
+                    password="my-cool-password",
+                    host="localhost",
+                    port="5432",
+                    dry_run=False,
+                    dump_file=None,
+                    init_sql="set work_mem='1GB'",
+                ),  # noqa
+                [
+                    call("SET search_path TO db;"),
+                    call("set work_mem='1GB'"),
+                    call('TRUNCATE TABLE "django_session"'),
+                    call('SELECT COUNT(*) FROM "auth_user"'),
+                    call(
+                        'SELECT "id", "first_name", "last_name", "email" FROM "auth_user"'
+                    ),
+                    call(
+                        'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n'
+                        '                    FROM "auth_user" WITH NO DATA'
+                    ),
+                    # noqa
+                    call('CREATE INDEX ON "tmp_auth_user" ("id")'),
+                    call(
+                        'UPDATE "auth_user" t '
+                        'SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" '
+                        'FROM "tmp_auth_user" s WHERE t."id" = s."id"'
+                    ),
+                ],
+                1,
+                [],
             ],
-            1,
-            [call('pg_dump -Fc -Z 9 -d db -U root -h localhost -p 5432 -f ./dump.sql', shell=True)]
+            [
+                "--dry-run --host localhost --port 5432 --user root --password my-cool-password "
+                "--dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql \"set work_mem='1GB'\"",
+                Namespace(
+                    verbose=1,
+                    list_providers=False,
+                    schema="./tests/schemes/valid_schema.yml",
+                    dbname="db",
+                    user="root",
+                    password="my-cool-password",
+                    host="localhost",
+                    port="5432",
+                    dry_run=True,
+                    dump_file=None,
+                    init_sql="set work_mem='1GB'",
+                ),  # noqa
+                [
+                    call("SET search_path TO db;"),
+                    call("set work_mem='1GB'"),
+                    call('TRUNCATE TABLE "django_session"'),
+                    call(
+                        'SELECT "id", "first_name", "last_name", "email" FROM "auth_user" LIMIT 100'
+                    ),
+                    call(
+                        'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n'
+                        '                    FROM "auth_user" WITH NO DATA'
+                    ),
+                    call('CREATE INDEX ON "tmp_auth_user" ("id")'),
+                    call(
+                        'UPDATE "auth_user" t '
+                        'SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" '
+                        'FROM "tmp_auth_user" s WHERE t."id" = s."id"'
+                    ),
+                ],
+                0,
+                [],
+            ],
+            [
+                "--dump-file ./dump.sql --host localhost --port 5432 --user root --password my-cool-password "
+                "--dbname db --schema ./tests/schemes/valid_schema.yml -v --init-sql \"set work_mem='1GB'\"",
+                # noqa
+                Namespace(
+                    verbose=1,
+                    list_providers=False,
+                    schema="./tests/schemes/valid_schema.yml",
+                    dbname="db",
+                    user="root",
+                    password="my-cool-password",
+                    host="localhost",
+                    port="5432",
+                    dry_run=False,
+                    dump_file="./dump.sql",
+                    init_sql="set work_mem='1GB'",
+                ),
+                [
+                    call("SET search_path TO db;"),
+                    call("set work_mem='1GB'"),
+                    call('TRUNCATE TABLE "django_session"'),
+                    call('SELECT COUNT(*) FROM "auth_user"'),
+                    call(
+                        'SELECT "id", "first_name", "last_name", "email" FROM "auth_user"'
+                    ),
+                    call(
+                        'CREATE TEMP TABLE "tmp_auth_user" AS SELECT "id", "first_name", "last_name", "email"\n'
+                        '                    FROM "auth_user" WITH NO DATA'
+                    ),
+                    call('CREATE INDEX ON "tmp_auth_user" ("id")'),
+                    call(
+                        'UPDATE "auth_user" t '
+                        'SET "first_name" = s."first_name", "last_name" = s."last_name", "email" = s."email" '
+                        'FROM "tmp_auth_user" s WHERE t."id" = s."id"'
+                    ),
+                ],
+                1,
+                [
+                    call(
+                        "pg_dump -Fc -Z 9 -d db -U root -h localhost -p 5432 -f ./dump.sql",
+                        shell=True,
+                    )
+                ],
+            ],
+            [
+                "--list-providers",
+                Namespace(
+                    verbose=None,
+                    list_providers=True,
+                    schema="schema.yml",
+                    dbname=None,
+                    user=None,
+                    password="",
+                    host="localhost",
+                    port="5432",
+                    dry_run=False,
+                    dump_file=None,
+                    init_sql=False,
+                ),
+                [],
+                0,
+                [],
+            ],
         ],
-
-        ['--list-providers',
-         Namespace(verbose=None, list_providers=True, schema='schema.yml', dbname=None, user=None,
-                   password='', host='localhost', port='5432', dry_run=False, dump_file=None, init_sql=False),
-         [], 0, []
-         ]
-    ])
-    def test_cli_args(self, subprocess, patched_connect, quote_ident, cli_args, expected, expected_executes,
-                      commit_calls, call_dump):  # noqa
+    )
+    def test_cli_args(
+        self,
+        subprocess,
+        patched_connect,
+        quote_ident,
+        cli_args,
+        expected,
+        expected_executes,
+        commit_calls,
+        call_dump,
+    ):
         arg_parser = get_arg_parser()
         parsed_args = arg_parser.parse_args(shlex.split(cli_args))
         assert parsed_args == expected
@@ -107,3 +183,19 @@ class TestCli:
         assert connection.commit.call_count == commit_calls
 
         assert subprocess.call.call_args_list == call_dump
+
+    @patch("pganonymize.utils.psycopg2.connect")
+    @pytest.mark.parametrize(
+        "cli_args",
+        [
+            "--host localhost --port 5432 --user root --password my-cool-password --dbname db "
+            "--schema ./tests/schemes/invalid_schema2.yml -v --init-sql \"set work_mem='1GB'\"",
+        ],
+    )
+    def test_invalid_schema(self, patched_connect, cli_args):
+        exc_text = "One of target_schema or overwrite_values_in_source_tables parameters must be defined!"
+        arg_parser = get_arg_parser()
+        parsed_args = arg_parser.parse_args(shlex.split(cli_args))
+        with pytest.raises(BadSchemaFormat) as exc_info:
+            main(parsed_args)
+        assert exc_info.value.args[0] == exc_text
--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,3 +1,4 @@
+import datetime
 import operator
 import uuid
 from collections import OrderedDict
@@ -7,49 +8,55 @@ import six
 from mock import MagicMock, Mock, patch
 
 from pganonymize import exceptions, providers
+from pganonymize.encrypting.encrypt_service import EncryptingService
+from pganonymize.exceptions import InvalidProviderArgument
 
 
 def test_register():
     registry = providers.ProviderRegistry()
 
-    @providers.register('foo', registry=registry)
+    @providers.register("foo", registry=registry)
     class FooProvider(providers.Provider):
-
         def alter_value(self, value):
-            return 'foo'
+            return "foo"
 
-    @providers.register('bar', registry=registry)
+    @providers.register("bar", registry=registry)
     class BarProvider(providers.Provider):
-
         def alter_value(self, value):
-            return 'bar'
+            return "bar"
 
     assert len(registry._registry) == 2
-    assert 'foo' in registry._registry
-    assert 'bar' in registry._registry
+    assert "foo" in registry._registry
+    assert "bar" in registry._registry
 
 
 class TestProviderRegistry:
-
     def test_constructor(self):
         registry = providers.ProviderRegistry()
         assert registry._registry == {}
 
-    @pytest.mark.parametrize('classes, expected', [
-        (
-            OrderedDict([
-                ('foo', Mock(spec=providers.Provider)),
-            ]),
-            ['foo']
-        ),
-        (
-            OrderedDict([
-                ('foo', Mock(spec=providers.Provider)),
-                ('bar', Mock(spec=providers.Provider)),
-            ]),
-            ['foo', 'bar']
-        )
-    ])
+    @pytest.mark.parametrize(
+        "classes, expected",
+        [
+            (
+                OrderedDict(
+                    [
+                        ("foo", Mock(spec=providers.Provider)),
+                    ]
+                ),
+                ["foo"],
+            ),
+            (
+                OrderedDict(
+                    [
+                        ("foo", Mock(spec=providers.Provider)),
+                        ("bar", Mock(spec=providers.Provider)),
+                    ]
+                ),
+                ["foo", "bar"],
+            ),
+        ],
+    )
     def test_register(self, classes, expected):
         registry = providers.ProviderRegistry()
         for key, cls in classes.items():
@@ -59,29 +66,36 @@ class TestProviderRegistry:
 
     def test_register_raises_exception(self):
         registry = providers.ProviderRegistry()
-        registry.register(Mock(), 'foo1')
-        registry.register(Mock(), 'foo2')
+        registry.register(Mock(), "foo1")
+        registry.register(Mock(), "foo2")
         with pytest.raises(exceptions.ProviderAlreadyRegistered):
-            registry.register(Mock(), 'foo1')
-            registry.register(Mock(), 'foo2')
+            registry.register(Mock(), "foo1")
+            registry.register(Mock(), "foo2")
 
-    @pytest.mark.parametrize('provider_id, effect', [
-        ('foooooo', pytest.raises(exceptions.InvalidProvider)),
-        ('foobar', pytest.raises(exceptions.InvalidProvider)),
-        ('barr', pytest.raises(exceptions.InvalidProvider)),
-        ('foo', MagicMock()),
-        ('bar', MagicMock()),
-        ('baz', MagicMock()),
-        ('baz.uuid', MagicMock()),
-    ])
+    @pytest.mark.parametrize(
+        "provider_id, effect",
+        [
+            ("foooooo", pytest.raises(exceptions.InvalidProvider)),
+            ("foobar", pytest.raises(exceptions.InvalidProvider)),
+            ("barr", pytest.raises(exceptions.InvalidProvider)),
+            ("foo", MagicMock()),
+            ("bar", MagicMock()),
+            ("baz", MagicMock()),
+            ("baz.uuid", MagicMock()),
+        ],
+    )
     def test_get_provider(self, provider_id, effect):
         provider = None
         registry = providers.ProviderRegistry()
-        with patch.object(registry, '_registry', {
-            'foo': Mock(spec=providers.Provider),
-            'bar': Mock(spec=providers.Provider),
-            'baz.*': Mock(spec=providers.Provider, regex_match=True)
-        }):
+        with patch.object(
+            registry,
+            "_registry",
+            {
+                "foo": Mock(spec=providers.Provider),
+                "bar": Mock(spec=providers.Provider),
+                "baz.*": Mock(spec=providers.Provider, regex_match=True),
+            },
+        ):
             with effect:
                 provider = registry.get_provider(provider_id)
         if provider is not None:
@@ -92,97 +106,153 @@ class TestProviderRegistry:
 
 
 class TestProvider:
-
     def test_alter_value(self):
         provider = providers.Provider()
         with pytest.raises(NotImplementedError):
-            provider.alter_value('Foo')
+            provider.alter_value("Foo")
 
 
 class TestChoiceProvider:
-
     def test_alter_value(self):
-        choices = ['Foo', 'Bar', 'Baz']
+        choices = ["Foo", "Bar", "Baz"]
         provider = providers.ChoiceProvider(values=choices)
         for choice in choices:
-            assert provider.alter_value(choice) in choices
+            assert provider.alter_value("any_value") in choices
+
+    def test_wrong_value(self):
+        choices = ["Foo", "Bar", "Baz"]
+        provider = providers.ChoiceProvider(values=choices)
+
+        bad_choices = ["Span"]
+
+        with pytest.raises(AssertionError) as _:
+            for choice in choices:
+                assert provider.alter_value("any_value") in bad_choices
 
 
 class TestClearProvider:
-
     def test_alter_value(self):
         provider = providers.ClearProvider()
-        assert provider.alter_value('Foo') is None
+        assert provider.alter_value("Foo") is None
 
 
 class TestFakeProvider:
-
-    @pytest.mark.parametrize('name, function_name', [
-        ('fake.first_name', 'first_name'),
-        ('fake.unique.first_name', 'unique.first_name'),
-    ])
-    @patch('pganonymize.providers.fake_data')
+    @pytest.mark.parametrize(
+        "name, function_name",
+        [
+            ("fake.first_name", "first_name"),
+            ("fake.unique.first_name", "unique.first_name"),
+        ],
+    )
+    @patch("pganonymize.providers.fake_data")
     def test_alter_value(self, mock_fake_data, name, function_name):
         provider = providers.FakeProvider(name=name)
-        provider.alter_value('Foo')
-        assert operator.attrgetter(function_name)(mock_fake_data).call_count == 1
+        provider.alter_value("Foo")
+        assert (
+            operator.attrgetter(function_name)(mock_fake_data).call_count == 1
+        )
 
-    @pytest.mark.parametrize('name', [
-        'fake.foo_name'
-    ])
+    @pytest.mark.parametrize("name", ["fake.foo_name"])
     def test_invalid_names(self, name):
         provider = providers.FakeProvider(name=name)
         with pytest.raises(exceptions.InvalidProviderArgument):
-            provider.alter_value('Foo')
+            provider.alter_value("Foo")
 
 
 class TestMaskProvider:
-
-    @pytest.mark.parametrize('value, sign, expected', [
-        ('Foo', None, 'XXX'),
-        ('Baaaar', '?', '??????'),
-    ])
+    @pytest.mark.parametrize(
+        "value, sign, expected",
+        [
+            ("Foo", None, "XXX"),
+            ("Baaaar", "?", "??????"),
+        ],
+    )
     def test_alter_value(self, value, sign, expected):
         provider = providers.MaskProvider(sign=sign)
         assert provider.alter_value(value) == expected
 
 
 class TestMD5Provider:
-
     def test_alter_value(self):
         provider = providers.MD5Provider()
-        value = provider.alter_value('foo')
+        value = provider.alter_value("foo")
         assert isinstance(value, six.string_types)
         assert len(value) == 32
 
     def test_as_number(self):
         provider = providers.MD5Provider(as_number=True)
-        value = provider.alter_value('foo')
+        value = provider.alter_value("foo")
         assert isinstance(value, six.integer_types)
         assert value == 985560
 
         provider = providers.MD5Provider(as_number=True, as_number_length=8)
-        value = provider.alter_value('foobarbazadasd')
+        value = provider.alter_value("foobarbazadasd")
         assert isinstance(value, six.integer_types)
         assert value == 45684001
 
 
 class TestSetProvider:
-
-    @pytest.mark.parametrize('kwargs, expected', [
-        ({'value': None}, None),
-        ({'value': 'Bar'}, 'Bar')
-    ])
+    @pytest.mark.parametrize(
+        "kwargs, expected",
+        [({"value": None}, None), ({"value": "Bar"}, "Bar")],
+    )
     def test_alter_value(self, kwargs, expected):
         provider = providers.SetProvider(**kwargs)
-        assert provider.alter_value('Foo') == expected
+        assert provider.alter_value("Foo") == expected
 
 
 class TestUUID4Provider:
-    @pytest.mark.parametrize('kwargs, expected', [
-        ({'value': None}, None),
-        ({'value': 'Bar'}, 'Bar')
-    ])
+    @pytest.mark.parametrize(
+        "kwargs, expected",
+        [({"value": None}, None), ({"value": "Bar"}, "Bar")],
+    )
     def test_alter_value(self, kwargs, expected):
         provider = providers.UUID4Provider(**kwargs)
-        assert type(provider.alter_value('Foo')) == uuid.UUID
+        assert type(provider.alter_value("Foo")) == uuid.UUID
+
+
+class TestKeepProvider:
+    def test_alter_value(self):
+        provider = providers.KeepProvider()
+        assert provider.alter_value("Foo") == "Foo"
+
+
+class TestPBKDF2Provider:
+    def test_alter_value(self):
+        PASSPHRASE = "secret_phrase"
+        SECRET_DATA = "some secret data"
+        provider = providers.PBKDF2Provider(secret=PASSPHRASE)
+
+        encrypted_data = provider.alter_value(SECRET_DATA)
+
+        decrypted_data = EncryptingService(PASSPHRASE).decrypt_function(
+            encrypted_data
+        )
+
+        assert decrypted_data == SECRET_DATA
+
+    def test_no_pbkdf2_passphrase(self):
+        provider = providers.PBKDF2Provider()
+        with pytest.raises(InvalidProviderArgument) as exc_info:
+            provider.alter_value("SECRET_DATA")
+
+        exc_text = 'attribute "secret" of pbkdf2 provider is not set'
+        assert exc_info.value.args[0] == exc_text
+
+    def test_missing_DA_SECRET_PHRASE_env_variable(self):
+        provider = providers.PBKDF2Provider(secret="DA_SECRET_PHRASE")
+        with pytest.raises(InvalidProviderArgument) as exc_info:
+            provider.alter_value("SECRET_DATA")
+
+        exc_text = (
+            "cannot find environment variable DA_SECRET_PHRASE "
+            "check your .env file"
+        )
+        assert exc_info.value.args[0] == exc_text
+
+
+class TestDatetimeProvider:
+    def test_alter_value(self):
+        provider = providers.DatetimeProvider()
+        result = provider.alter_value("testvalue")
+        assert type(result) == datetime.date
